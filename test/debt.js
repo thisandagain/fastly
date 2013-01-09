b984217cb703237dcafb10e91f07a77aaac9014d
Andrew Sliwinski <thisandagain@gmail.com>
2013-01-08 16:33:42 -0800

    Lower debt limit (no greater than 20 codebux)

--- test/debt.js
+++ test/debt.js
@@ -18,7 +18,7 @@ bux(__dirname + '/../lib', function (err, obj) {
     test('Debt', function (t) {
         t.equal(err, null, 'Errors should be null');
         t.type(obj, 'number', 'Results should be a number');
-        t.ok(obj > 50, 'Total should be greater than 50');
+        t.ok(obj > 80, 'Total should be greater than 80');
         t.end();
     });
 });
